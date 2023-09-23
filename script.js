31882e24baeffb2405c9e29e6409b20368f4b9ca
Diogo S. Florêncio <diogosflorencio@gmail.com>
2023-09-23 16:20:59 -0300

    Adicionei funções nos botões, conteúdos em cada um e fiz algumas melhorias gerais

--- script.js
+++ script.js
@@ -7,4 +7,43 @@
 
 
 /*exibindo conteudo sobre mim*/
+/* esse código ficou extremamente simples e repetitivo. foi a primeira coisa que veio a mente, logo melhorarei*/
 
+function mostrarSobreMim(){
+    ocultarformacao();
+    ocultarOcultarPortfolio();
+    ocultarOcultarContato();
+    return document.getElementById('sobremim').style.display='block';
+}
+function mostrarFormacao(){
+    ocultarSobreMim();
+    ocultarOcultarPortfolio();
+    ocultarOcultarContato();
+    return document.getElementById('formacao').style.display='block';
+}
+function mostrarPortfolio(){
+    ocultarSobreMim();
+    ocultarformacao();
+    ocultarOcultarContato();
+    return document.getElementById('portfolio').style.display='block';
+}
+function mostrarContato(){
+    ocultarSobreMim();
+    ocultarformacao();
+    ocultarOcultarPortfolio();
+    return document.getElementById('contato').style.display='block';
+}
+
+/*ocultando conteudos*/
+function ocultarSobreMim(){
+    return document.getElementById('sobremim').style.display='none';
+}
+function ocultarformacao(){
+    return document.getElementById('formacao').style.display='none';
+}
+function ocultarOcultarPortfolio(){
+    return document.getElementById('portfolio').style.display='none';
+}
+function ocultarOcultarContato(){
+    return document.getElementById('contato').style.display='none';
+}
