472014b363366c9846cd12ee368cd97bd0349755
Diogo S. Florêncio <diogosflorencio@gmail.com>
2023-09-23 20:00:59 -0300

    inicia no "sobre mim"

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /*exibindo conteudo sobre mim*/
 /* esse código ficou extremamente simples e repetitivo. foi a primeira coisa que veio a mente, logo melhorarei*/
-
 function mostrarSobreMim(){
     ocultarformacao();
     ocultarOcultarPortfolio();
@@ -39,16 +38,6 @@ function ocultarOcultarPortfolio(){
 function ocultarOcultarContato(){
     return document.getElementById('contato').style.display='none';
 }
-
-const canvas = document.querySelector(".conteudo-padrao");
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "rgb(0, 0, 0)";
-ctx.fillRect(0, 0, width, height);
-ctx.fillStyle = "rgb(255, 0, 0, 2)";
-ctx.fillRect(50, 50, 100, 150);
-
 
 
 
