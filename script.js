9db120758c243fc10b2e9c479851fa1dc86b324e
Diogo S. Florêncio <diogosflorencio@gmail.com>
2023-09-23 17:59:43 -0300

    Quase finalizando. Estilizei as informações

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/*bolinhas conteúdo padrão*/
-
-
-
-
-
-
-
 /*exibindo conteudo sobre mim*/
 /* esse código ficou extremamente simples e repetitivo. foi a primeira coisa que veio a mente, logo melhorarei*/
 
@@ -47,3 +39,17 @@ function ocultarOcultarPortfolio(){
 function ocultarOcultarContato(){
     return document.getElementById('contato').style.display='none';
 }
+
+const canvas = document.querySelector(".conteudo-padrao");
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "rgb(0, 0, 0)";
+ctx.fillRect(0, 0, width, height);
+ctx.fillStyle = "rgb(255, 0, 0, 2)";
+ctx.fillRect(50, 50, 100, 150);
+
+
+
+
+
